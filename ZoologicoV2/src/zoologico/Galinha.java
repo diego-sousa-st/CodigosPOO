81277836cdabaf68b3e96c145fcdb66f360f979b
diego-sousa-st <diegosousa.st@gmail.com>
2016-12-10 11:29:51 -0200

    Código do zoologico usando classes abstratas

---
+++ ZoologicoV2/src/zoologico/Galinha.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoologico;
+
+/**
+ *
+ * @author diego
+ */
+public class Galinha extends Ave{
+    
+    
+    public Galinha(String nome){
+        super(false, nome, "Galinha", 2, "Cocoricó!");
+    }
+    
+    @Override
+    public String getDescricao(){
+        return "uma "+super.getEspecie();
+    }
+}
